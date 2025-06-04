8f8886800a6d217a412640984ae8e5c4fa7f3b29
OVO <lzx213@sina.com>
2025-06-04 10:55:08 +0800

    Update new camera

--- Assets/Scripts/Manager/ControllableManager.cs
+++ Assets/Scripts/Manager/ControllableManager.cs
@@ -57,7 +57,7 @@ public class ControllableManager : BaseManager<ControllableManager>
 
         currentControllable = newControllable;
 
-        CameraManager.Instance.SmoothBlendTo(currentControllable.GetLookAt());
+        //CameraManager.Instance.SmoothBlendTo(currentControllable.GetLookAt());
     }
 
     public BaseControllable GetPlayerControllable()
@@ -67,9 +67,9 @@ public class ControllableManager : BaseManager<ControllableManager>
 
     private void ActivateCurrControllable()
     {
-        CameraManager.Instance.SetUpFollowPoint(currentControllable.GetLookAt());
+        //CameraManager.Instance.SetUpFollowPoint(currentControllable.GetLookAt());
         currentControllable.SetControl(true);
         currentControllable.EnableControl();
-        CameraManager.Instance.DeactiveDummy();
+        //CameraManager.Instance.DeactiveDummy();
     }
 }
