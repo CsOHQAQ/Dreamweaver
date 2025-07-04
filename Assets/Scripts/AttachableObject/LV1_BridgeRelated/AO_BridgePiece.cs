f10410f6e6f42aa527e77fd16cada09b682438d8
Taihou <ansonfeng1999@gmail.com>
2025-07-04 16:34:35 +0800

    Implement rotation

--- Assets/Scripts/AttachableObject/LV1_BridgeRelated/AO_BridgePiece.cs
+++ Assets/Scripts/AttachableObject/LV1_BridgeRelated/AO_BridgePiece.cs
@@ -1,24 +1,26 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class AO_BridgePiece : AttachableObject
 {
-    private RopeObject ropeObject;
-    [SerializeField] private Transform originPos;
+    [SerializeField] private RopeObject ropeObject;
+    [SerializeField] private Transform targetPos;
     [SerializeField][Range(0f, 5f)] private float smoothTime = 0.3f;
 
     [SerializeField] private Vector3 velocity = Vector3.zero;
+    [SerializeField] private float angleVelocity = 0f;
     private Coroutine moveCoroutine;
 
-    public event Action OnArriveAtOrigin = () => Debug.Log("Move Finished");
+    public event Action OnArriveAtTarget = () => Debug.Log("Move Finished");
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("BridgeTrigger") && !gameObject.CompareTag("Player"))
         {
-            CutRope(ropeObject);
+            CutRope();
             gameObject.layer = LayerMask.NameToLayer("Ground");
             gameObject.GetComponent<Rigidbody>().useGravity = false;
             Movable = false;
@@ -39,27 +41,45 @@ public class AO_BridgePiece : AttachableObject
         ropeObject = null;
     }
 
-    private void CutRope(RopeObject rope)
+    private void CutRope()
     {
-        rope.InstantBreak();
+        if (ropeObject == null) return;
+        var rope = ropeObject;
         RopeObjectUnset();
+        rope.InstantBreak();
     }
 
     private IEnumerator MoveToPosition()
     {
-
-        while (Vector3.Distance(originPos.position, gameObject.transform.position) > 0.01f)
+        Vector3 originalEuler = transform.eulerAngles;
+        while (Vector3.Distance(targetPos.position, gameObject.transform.position) > 0.01f)
         {
-            transform.position = Vector3.SmoothDamp(
-                transform.position,
-                originPos.position,
-                ref velocity,
+            if (Vector3.Distance(targetPos.position, gameObject.transform.position) > 0.01f)
+            {
+                transform.position = Vector3.SmoothDamp(
+                    transform.position,
+                    targetPos.position,
+                    ref velocity,
+                    smoothTime
+                );
+            }
+
+            // --- Smooth Rotation ---
+            float targetY = targetPos.GetComponentInParent<Transform>().eulerAngles.y;
+            float newY = Mathf.SmoothDampAngle(
+                transform.eulerAngles.y,
+                targetY,
+                ref angleVelocity,
                 smoothTime
             );
+
+            transform.rotation = Quaternion.Euler(originalEuler.x, newY, originalEuler.z);
             yield return null;
         }
-        gameObject.transform.position = originPos.position;
+        Debug.Log($"Target angle of y is: {targetPos.GetComponentInParent<Transform>().eulerAngles.y}");
+        // gameObject.transform.SetPositionAndRotation(targetPos.position, Quaternion.LookRotation(targetPos.GetComponentInParent<Transform>().forward));
         velocity = Vector3.zero;
-        OnArriveAtOrigin?.Invoke();
+        angleVelocity = 0f;
+        OnArriveAtTarget?.Invoke();
     }
 }
